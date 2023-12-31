0cd9ccde8cda2e21ea48e99df1d05c113b15d85c
Huzaifa Jawad <hjawad.bscs21seecs@seecs.edu.pk>
2024-01-01 02:30:30 +0500

    Adding packet to unacknowledged window

--- sender/sender.c
+++ sender/sender.c
@@ -17,6 +17,13 @@ struct packet {
     int32_t len;
 };
 
+// Define a structure for window elements
+struct window_element {
+    struct packet pkt;
+    int acknowledged;
+    time_t send_time;
+};
+
 // Function to serialize the packet
 void serialize_packet(struct packet *pkt, char *buffer) {
     int32_t seq_num_net = htonl(pkt->seq_num); // Convert to network byte order
@@ -28,9 +35,13 @@ void serialize_packet(struct packet *pkt, char *buffer) {
 }
 
 // Function to check if the window is full
-int is_window_full(int base, int next_seq_num) {
-    return next_seq_num >= base + WINDOW_SIZE;
+int is_window_full(int window_start, int window_end) {
+    return window_end - window_start >= WINDOW_SIZE;
 }
+
+struct window_element window[WINDOW_SIZE]; // Window array
+int window_start = 0; // Start of the window
+int window_end = 0;   // End of the window (next position to insert)
 
 int main() {
     // Initialize socket
@@ -59,12 +70,10 @@ int main() {
     }
 
     int seq_num = 0;
-    int base = 0;
-    int next_seq_num = 0;
 
     // Main loop to read from the file and send packets
     while (!feof(file)) {
-        if (!is_window_full(base, next_seq_num)) {
+        if (!is_window_full(window_start, window_end)) {
             struct packet pkt;
             pkt.seq_num = seq_num;
             pkt.len = fread(pkt.data, 1, PACKET_SIZE, file);
@@ -78,23 +87,20 @@ int main() {
             char serialized_pkt[sizeof(struct packet)];
             serialize_packet(&pkt, serialized_pkt);
 
+            // TODO: Send the packet
             ssize_t sent_bytes = sendto(sockfd, serialized_pkt, sizeof(serialized_pkt), 0, (const struct sockaddr *)&servaddr, sizeof(servaddr));
             if (sent_bytes < 0) {
                 perror("Failed to send packet");
                 exit(EXIT_FAILURE);
             }
 
-            // TODO: Send the packet
-            ssize_t sent_bytes = sendto(sockfd, (const char *)&pkt, sizeof(pkt), 0, (const struct sockaddr *)&servaddr, sizeof(servaddr));
-            if (sent_bytes < 0) {
-                perror("Failed to send packet");
-                exit(EXIT_FAILURE);
-            }
-
             // TODO: Add packet to unacknowledged window
-            // ...
+            window[window_end % WINDOW_SIZE].pkt = pkt;
+            window[window_end % WINDOW_SIZE].acknowledged = 0;
+            window[window_end % WINDOW_SIZE].send_time = time(NULL);
+            window_end++;
 
-            next_seq_num++;
+            seq_num++;
         }
 
         // TODO: Process any received acknowledgments
