3ddccedde653ac4f934725eb53cb70374c598db1
Huzaifa Jawad <hjawad.bscs21seecs@seecs.edu.pk>
2024-01-01 04:18:13 +0500

    Adding to the receiver code

--- reciever/reviever.c
+++ reciever/reviever.c
@@ -4,46 +4,91 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
 #define PACKET_SIZE 500
-#define WINDOW_SIZE 10  // Example window size
+#define PORT 8080
 
-// Function to process packets and write to file
-// ...
+// Packet structure definition
+struct packet {
+    int32_t seq_num;
+    char data[PACKET_SIZE];
+    int32_t len;
+};
+
+// Function to deserialize the packet
+void deserialize_packet(char *buffer, struct packet *pkt) {
+    int32_t seq_num_net, len_net;
+
+    memcpy(&seq_num_net, buffer, sizeof(seq_num_net));
+    pkt->seq_num = ntohl(seq_num_net); // Convert from network byte order
+
+    memcpy(pkt->data, buffer + sizeof(seq_num_net), PACKET_SIZE);
+
+    memcpy(&len_net, buffer + sizeof(seq_num_net) + PACKET_SIZE, sizeof(len_net));
+    pkt->len = ntohl(len_net); // Convert from network byte order
+}
+
+// Function to send acknowledgment
+void send_acknowledgment(int sockfd, struct sockaddr_in *sender_addr, int32_t ack_seq_num) {
+    int32_t ack_seq_num_net = htonl(ack_seq_num);
+    sendto(sockfd, &ack_seq_num_net, sizeof(ack_seq_num_net), 0, (struct sockaddr *)sender_addr, sizeof(*sender_addr));
+}
 
 int main() {
-    // Initialize socket
-    // ...
+    int sockfd;
+    struct sockaddr_in servaddr, sender_addr;
+    socklen_t sender_addr_len = sizeof(sender_addr);
 
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&sender_addr, 0, sizeof(sender_addr));
+
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
+    // Bind the socket with the server address
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[sizeof(struct packet)];
+    struct packet pkt;
+    
     // Open the file for writing
-    // ...
+    FILE *file = fopen("output.mp4", "wb");
+    if (file == NULL) {
+        perror("Failed to open file");
+        exit(EXIT_FAILURE);
+    }
 
-    // Main loop to receive packets
     while (1) {
-        char buffer[PACKET_SIZE];
+        ssize_t len = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&sender_addr, &sender_addr_len);
+        if (len > 0) {
+            deserialize_packet(buffer, &pkt);
 
-        ////////////////////////// Your code for Receiving Packet here /////////////////////////////////
-        // ...
+            // TODO: Process packet (Reorder based on sequence number, write to file, etc.)
+            // ...
 
-        ////////////////////////// Your code for Sequence Number Checking here /////////////////////////
-        // ...
+            // Send acknowledgment
+            send_acknowledgment(sockfd, &sender_addr, pkt.seq_num);
+        }
 
-        ////////////////////////// Your code for Acknowledgment Sending here ///////////////////////////
-        // ...
-
-        ////////////////////////// Your code for Re-ordering here //////////////////////////////////////
-        // ...
-
-        // Write the processed packet to the file
-        // ...
-
-        // Check for end of file transfer
+        // TODO: Check for end of transmission and break the loop
         // ...
     }
 
     // Close file and socket
-    // ...
+    fclose(file);
+    close(sockfd);
 
-    // Terminate receiver
     return 0;
 }
