d2f2a747930e6bfc19341f62e9c08182723d98e6
Huzaifa Jawad <hjawad.bscs21seecs@seecs.edu.pk>
2024-01-01 03:21:22 +0500

    Adding acknowledgement handling window updating and readme file

--- sender/sender.c
+++ sender/sender.c
@@ -5,10 +5,12 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <fcntl.h> // For non-blocking socket
 
 #define PACKET_SIZE 500
 #define WINDOW_SIZE 10  // Example window size
 #define PORT 8080
+#define RETRANSMISSION_TIMEOUT 2 // Timeout in seconds
 
 // Packet structure definition
 struct packet {
@@ -22,6 +24,11 @@ struct window_element {
     struct packet pkt;
     int acknowledged;
     time_t send_time;
+};
+
+// Define a structure for acknowledgments
+struct ack_packet {
+    int32_t ack_seq_num;
 };
 
 // Function to serialize the packet
@@ -60,6 +67,18 @@ int main() {
     servaddr.sin_family = AF_INET; // IPv4
     servaddr.sin_port = htons(PORT); // Port number
     servaddr.sin_addr.s_addr = INADDR_ANY; // For localhost
+
+    // Set the socket to non-blocking mode
+    int flags = fcntl(sockfd, F_GETFL, 0);
+    if (flags < 0) {
+        perror("Failed to get socket flags");
+        exit(EXIT_FAILURE);
+    }
+
+    if (fcntl(sockfd, F_SETFL, flags | O_NONBLOCK) < 0) {
+        perror("Failed to set socket to non-blocking");
+        exit(EXIT_FAILURE);
+    }
 
     // Open the file
     char *filename = "input.mp4";
@@ -103,14 +122,46 @@ int main() {
             seq_num++;
         }
 
-        // TODO: Process any received acknowledgments
-        // ...
+        // Process any received acknowledgments
+        struct ack_packet ack;
+        struct sockaddr_in from;
+        socklen_t fromlen = sizeof(from);
+        ssize_t ack_len;
 
-        // TODO: Handle retransmission if needed
-        // ...
+        while ((ack_len = recvfrom(sockfd, &ack, sizeof(ack), 0, (struct sockaddr *)&from, &fromlen)) > 0) {
+            int ack_seq_num = ntohl(ack.ack_seq_num);
 
-        // TODO: Update window and check for transmission completion
-        // ...
+            // Mark packets as acknowledged
+            for (int i = window_start; i != window_end; i = (i + 1) % WINDOW_SIZE) {
+                if (window[i % WINDOW_SIZE].pkt.seq_num == ack_seq_num) {
+                    window[i % WINDOW_SIZE].acknowledged = 1;
+                    break;
+                }
+            }
+
+            // Move window start
+            while (window[window_start % WINDOW_SIZE].acknowledged) {
+                window_start = (window_start + 1) % WINDOW_SIZE;
+            }
+        }
+
+        // Handle retransmission if needed
+        time_t current_time = time(NULL);
+        for (int i = window_start; i != window_end; i = (i + 1) % WINDOW_SIZE) {
+            if (!window[i % WINDOW_SIZE].acknowledged && difftime(current_time, window[i % WINDOW_SIZE].send_time) > RETRANSMISSION_TIMEOUT) {
+                // Retransmit the packet
+                char serialized_pkt[sizeof(struct packet)];
+                serialize_packet(&window[i % WINDOW_SIZE].pkt, serialized_pkt);
+                sendto(sockfd, serialized_pkt, sizeof(serialized_pkt), 0, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+                window[i % WINDOW_SIZE].send_time = current_time;
+            }
+        }
+
+        // Update window and check for transmission completion
+        if (feof(file) && window_start == window_end) {
+            // All data sent and acknowledged
+            break;
+        }
     }
 
     // Close file and socket
